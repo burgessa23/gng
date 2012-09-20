3d6be7232b9ef3038376d4d7e53e2f576c37652e
Andrew Burgess <speed4@gmail.com>
2012-09-20 13:35:16 -0700

    better parsing

--- scripts/utils.js
+++ scripts/utils.js
@@ -5,20 +5,25 @@ google.setOnLoadCallback(initialize);
 // parse the feed and append the results to the
 function initialize() {
 	var feed = new google.feeds.Feed(feedURL);
-	feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
+	feed.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
 	feed.setNumEntries(10);
 	feed.load(function(result) {
 		if (!result.error) {
 			for (var i = 0; i < result.feed.entries.length; i++) {
 				var entry = result.feed.entries[i];
-				var fmtDate = entry.publishedDate.substring(0, parseInt(entry.publishedDate.length-15, 10));
-				$('#feedWrapper').append(
-					'<div class="feedContent" id="articleNum'+i+'">'
-					+'<p class="title">'+entry.title+'</p>'
-					+'<p class="date">'+fmtDate+'</p>'
-					+'<div class="mainContent">'+entry.content+'</div>'
-					+'</div>'
-				);
+				// if($.inArray('gng', entry.categories) !== -1) {
+					var fmtDate = entry.publishedDate.substring(0, parseInt(entry.publishedDate.length-15, 10));
+					var entryImageUrl = $(entry.content).find('img').eq(0).attr('src');
+					$('#feedWrapper').append(
+						'<div class="feedContent" id="articleNum'+i+'">'
+						+'<p class="title"><a href="'+entry.link+'">'+entry.title+'</a></p>'
+						+'<p class="date">'+fmtDate+'</p>'
+						+'<p>'+entry.contentSnippet+'</p>'
+						//+'<img src="'+entryImageUrl+'" />'
+						//+'<div class="mainContent">'+entry.content+'</div>'
+						+'</div>'
+					);
+				// }
 			}
 		}
 	});
