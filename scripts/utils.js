b83285850a804787745b08ab45d828226da0dd23
Andrew Burgess <speed4@gmail.com>
2012-09-20 16:42:33 -0700

    fjsdjk

--- scripts/utils.js
+++ scripts/utils.js
@@ -14,17 +14,13 @@ function initialize() {
 				// if($.inArray('gng', entry.categories) !== -1) {
 					var fmtDate = entry.publishedDate.substring(0, parseInt(entry.publishedDate.length-15, 10));
 					var entryImageUrl = $(entry.content).find('img').eq(0).attr('src');
-					$('#feedWrapper').append(
-						'<div class="feedContent" id="articleNum'+i+'">'
-						+'<p class="title"><a href="'+entry.link+'">'+entry.title+'</a></p>'
-						+'<p class="date">'+fmtDate+'</p>'
-						+'<p>'+entry.contentSnippet+'</p>'
-						//+'<img src="'+entryImageUrl+'" />'
-						//+'<div class="mainContent">'+entry.content+'</div>'
-						+'</div>'
-					);
+					$('#post_list').append(
+						'<li id="articeNum'+i+'"><div class="wrapper"><div class="thumb_container"><a class="thumbLink" href="'+entry.link+'"><img class="thumb" src="'+entryImageUrl+'" width="60" alt=""></a></div></div><div class="text"><span class="date" style="opacity: 1; display: block;">'+fmtDate+'</span><a class="headline" href="'+entry.link+'"><span class="headline-txt">'+entry.title+'</span></a></div><div class="clear"></div></li>'
+						);
 				// }
 			}
+		} else {
+			$('#feedWrapper').html("oops, something went wrong with our feed, please click <a href='http://www.sydneylovesfashion.com/'>here</a> to visit Sydney's blog");
 		}
 	});
 }
